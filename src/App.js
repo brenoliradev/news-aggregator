30437defd4f112c9f4339470367af46bfbf1dd99
lirabreno <github.lira@outlook.com>
2021-10-17 00:21:42 -0300

    fixed gramatical error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react"
-import FeedPage from "./compoents/feedPage/feedPage";
-import NewsAggregator from "./compoents/newsAggregator/newsAggregator";
+import FeedPage from "./components/feedPage/feedPage";
 
+<<<<<<< HEAD
+import './components/newsSearcher/newsSearcher.scss'
+=======
 import './compoents/feedPage/feedPage.scss';
+>>>>>>> parent of a19a2d8 (fix gramatical error)
 
 const App = () => {
     return (
